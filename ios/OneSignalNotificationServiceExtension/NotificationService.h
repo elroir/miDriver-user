8653296bed879b7b44bd9305adb634d9590584b6
Enrique Eid galarza <enrique@Enriques-Mac-mini.local>
2023-08-31 22:43:28 -0300

    iOS podfile change

---
+++ ios/OneSignalNotificationServiceExtension/NotificationService.h
@@ -0,0 +1,12 @@
+//
+//  NotificationService.h
+//  OneSignalNotificationServiceExtension
+//
+//  Created by Enrique Eid galarza on 31/8/23.
+//
+
+#import <UserNotifications/UserNotifications.h>
+
+@interface NotificationService : UNNotificationServiceExtension
+
+@end
